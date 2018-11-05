0a128fb881345056441f49ff3c9b952b02eead4c
Tahir <171048134@q.com>
2018-11-05 15:31:35 +0800

    添加注释
    添加注释

--- honey-back/Honeypot/src/main/java/com/honeypot/honeypot/dao/PotDao.java
+++ honey-back/Honeypot/src/main/java/com/honeypot/honeypot/dao/PotDao.java
@@ -8,9 +8,28 @@ import java.util.List;
 
 @Mapper
 public interface PotDao {
+    /**
+     * 根据蜜罐的类型获取蜜罐信息
+     * @param Type
+     * @return
+     */
     List<Pot> getPotByType(@Param("type") String type);
+    /**
+     * 根据主机蜜罐的类型获取主机蜜罐信息
+     * @param Type
+     * @return
+     */
     List<Pot> getHostPotByType(@Param("type") String type);
+    /**
+     * 根据蜜罐的IP获取蜜罐信息
+     * @param Type
+     * @return
+     */
     List<Pot> getPotByIp(@Param("ip") String ip);
-    // 删除一个用户
-    public boolean delPot(String ip);
+    /**
+     * 根据主机蜜罐的类型获取蜜罐信息
+     * @param uniqueId
+     * @return
+     */
+    public boolean delPot(String uniqueId);
 }
